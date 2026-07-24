3202d1fad6c2da71c79b3070e56a1800c30c83a5
Tom Boullay <tom@famille-boullay.fr>
2026-07-24 12:39:40 +0200

    fix: github pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
+import { env } from "node:process";
 import { defineConfig } from "astro/config";
+
+// Temp while vitrine site is on GitHub Pages
+const base = env.PUBLIC_BASE_PATH;
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://b0tastro.github.io",
-  base: "/portfolio-Maeva",
+  base,
 });
